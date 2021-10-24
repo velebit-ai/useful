4eb09d1dd49e819d2b207c4a9459e9ed0af1327c
Tomislav Levanic <tomo.levanic@gmail.com>
2021-10-24 21:52:40 +0200

    Upgrade the retry decorator to allow inf retries and retry/final callbacks

--- useful/decorators/__init__.py
+++ useful/decorators/__init__.py
@@ -6,7 +6,8 @@ from functools import wraps
 _log = logging.getLogger(__name__)
 
 
-def retry(retries=1, delay=0, exception=Exception):
+def retry(retries=1, delay=0, exception=Exception, retry_callback=None,
+          final_callback=None):
     """
     Decorator that retries given function multiple times with specified delay
     between re-runs.
@@ -16,25 +17,44 @@ def retry(retries=1, delay=0, exception=Exception):
         delay (float): seconds to delay between re-runs
         exception (class): specify exception type to retry on. Defaults to
             Exception.
+        retry_callback (function): function to call every time after wrapped
+            function fails with `exception` and retry will happen. The function
+            prototype is `function(e, *args, **kwargs)` where e is the raised
+            exception, and args and kwargs argument passed to the original func
+        final_callback (function): function to call after wrapped fails with
+            `exception` for the last time and retry won't happen. The function
+            prototype is `function(e, *args, **kwargs)` where e is the raised
+            exception, and args and kwargs argument passed to the original func
     """
-    assert retries >= 0
     assert delay >= 0
+    if retries < 0:
+        retries = float('+inf')
 
     def decorator(func):
+        @wraps(func)
         def function(*args, **kwargs):
-            for i in range(retries + 1):
+            call_count = 0
+            max_calls = 1 + retries
+            while call_count < max_calls:
                 try:
+                    call_count += 1
                     return func(*args, **kwargs)
                 except exception as e:
-                    if i == retries:
+                    m = f"[{call_count} / {max_calls}] Caught error '{e}'."
+
+                    if call_count == max_calls:
                         _log.warning(
-                            f"[{retries + 1} / {retries + 1}] All retries "
-                            f"failed. Caught error '{str(e)}'. Not retrying"
-                            f"anymore.", exc_info=True)
+                           f"{m} All retries failed. Not retrying anymore.",
+                           exc_info=True)
+
+                        if final_callback is not None:
+                            final_callback(e, *args, **kwargs)
                         raise
                     _log.warning(
-                        f"[{i + 1} / {retries + 1}] Caught error '{str(e)}', "
-                        f"retrying in {delay} seconds", exc_info=True)
+                       f"{m} Retrying in {delay} seconds", exc_info=True)
+
+                    if retry_callback is not None:
+                        retry_callback(e, *args, **kwargs)
                     time.sleep(delay)
         return function
     return decorator
