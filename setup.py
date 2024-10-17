f2a49eec335d1d04f2e2808c2133387bcdb60426
ssopar <simun.sopar@velebit.ai>
2024-10-17 14:25:42 +0000

    Made config loading compatible with Python 3.12

--- setup.py
+++ setup.py
@@ -88,6 +88,10 @@ setup(
         "Programming Language :: Python :: 3.6"
         "Programming Language :: Python :: 3.7"
         "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.11"
+        "Programming Language :: Python :: 3.12"
     ],
     url="https://github.com/velebit-ai/useful",
     author="Velebit AI",
